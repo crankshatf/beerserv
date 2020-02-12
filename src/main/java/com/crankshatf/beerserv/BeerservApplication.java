87eb3c19d13c47b8dded3ff970f5c2be35f5365b
Crankshaft <crankshatf@outlook.com>
2020-02-12 22:35:27 +0800

    Creating initial project. Closes #1

---
+++ src/main/java/com/crankshatf/beerserv/BeerservApplication.java
@@ -0,0 +1,13 @@
+package com.crankshatf.beerserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crankshatf/beerserv/BeerservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crankshatf.beerserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
